a96b2a291590e11db8087ff470a6b4ad079c5f90
Geoffrey0824 <geoffrey0824@sina.com>
2015-01-01 14:52:26 +0800

    Create GF256_with_Sbox_Linear_Analysis.py

---
+++ GF256_with_Sbox_Linear_Analysis.py
@@ -0,0 +1,252 @@
+INV = [0, 1, 141, 246, 203, 82, 123, 209, 232, 79, 41, 192, 176, 225, 229, 199, 116, 180, 170, 75, 153, 43, 96, 95, 88, 63, 253, 204, 255, 64, 238, 178, 58, 110, 90, 241, 85, 77, 168, 201, 193, 10, 152, 21, 48, 68, 162, 194, 44, 69, 146, 108, 243, 57, 102, 66, 242, 53, 32, 111, 119, 187, 89, 25, 29, 254, 55, 103, 45, 49, 245, 105, 167, 100, 171, 19, 84, 37, 233, 9, 237, 92, 5, 202, 76, 36, 135, 191, 24, 62, 34, 240, 81, 236, 97, 23, 22, 94, 175, 211, 73, 166, 54, 67, 244, 71, 145, 223, 51, 147, 33, 59, 121, 183, 151, 133, 16, 181, 186, 60, 182, 112, 208, 6, 161, 250, 129, 130, 131, 126, 127, 128, 150, 115, 190, 86, 155, 158, 149, 217, 247, 2, 185, 164, 222, 106, 50, 109, 216, 138, 132, 114, 42, 20, 159, 136, 249, 220, 137, 154, 251, 124, 46, 195, 143, 184, 101, 72, 38, 200, 18, 74, 206, 231, 210, 98, 12, 224, 31, 239, 17, 117, 120, 113, 165, 142, 118, 61, 189, 188, 134, 87, 11, 40, 47, 163, 218, 212, 228, 15, 169, 39, 83, 4, 27, 252, 172, 230, 122, 7, 174, 99, 197, 219, 226, 234, 148, 139, 196, 213, 157, 248, 144, 107, 177, 13, 214, 235, 198, 14, 207, 173, 8, 78, 215, 227, 93, 80, 30, 179, 91, 35, 56, 52, 104, 70, 3, 140, 221, 156, 125, 160, 205, 26, 65, 28]
+AFF = [31, 62, 124, 248, 241, 227, 199, 143]
+EXP = [1, 3, 5, 15, 17, 51, 85, 255, 26, 46, 114, 150, 161, 248, 19, 53, 95, 225, 56, 72, 216, 115, 149, 164, 247, 2, 6, 10, 30, 34, 102, 170, 229, 52, 92, 228, 55, 89, 235, 38, 106, 190, 217, 112, 144, 171, 230, 49, 83, 245, 4, 12, 20, 60, 68, 204, 79, 209, 104, 184, 211, 110, 178, 205, 76, 212, 103, 169, 224, 59, 77, 215, 98, 166, 241, 8, 24, 40, 120, 136, 131, 158, 185, 208, 107, 189, 220, 127, 129, 152, 179, 206, 73, 219, 118, 154, 181, 196, 87, 249, 16, 48, 80, 240, 11, 29, 39, 105, 187, 214, 97, 163, 254, 25, 43, 125, 135, 146, 173, 236, 47, 113, 147, 174, 233, 32, 96, 160, 251, 22, 58, 78, 210, 109, 183, 194, 93, 231, 50, 86, 250, 21, 63, 65, 195, 94, 226, 61, 71, 201, 64, 192, 91, 237, 44, 116, 156, 191, 218, 117, 159, 186, 213, 100, 172, 239, 42, 126, 130, 157, 188, 223, 122, 142, 137, 128, 155, 182, 193, 88, 232, 35, 101, 175, 234, 37, 111, 177, 200, 67, 197, 84, 252, 31, 33, 99, 165, 244, 7, 9, 27, 45, 119, 153, 176, 203, 70, 202, 69, 207, 74, 222, 121, 139, 134, 145, 168, 227, 62, 66, 198, 81, 243, 14, 18, 54, 90, 238, 41, 123, 141, 140, 143, 138, 133, 148, 167, 242, 13, 23, 57, 75, 221, 124, 132, 151, 162, 253, 28, 36, 108, 180, 199, 82, 246]
+LOG = [0, 25, 1, 50, 2, 26, 198, 75, 199, 27, 104, 51, 238, 223, 3, 100, 4, 224, 14, 52, 141, 129, 239, 76, 113, 8, 200, 248, 105, 28, 193, 125, 194, 29, 181, 249, 185, 39, 106, 77, 228, 166, 114, 154, 201, 9, 120, 101, 47, 138, 5, 33, 15, 225, 36, 18, 240, 130, 69, 53, 147, 218, 142, 150, 143, 219, 189, 54, 208, 206, 148, 19, 92, 210, 241, 64, 70, 131, 56, 102, 221, 253, 48, 191, 6, 139, 98, 179, 37, 226, 152, 34, 136, 145, 16, 126, 110, 72, 195, 163, 182, 30, 66, 58, 107, 40, 84, 250, 133, 61, 186, 43, 121, 10, 21, 155, 159, 94, 202, 78, 212, 172, 229, 243, 115, 167, 87, 175, 88, 168, 80, 244, 234, 214, 116, 79, 174, 233, 213, 231, 230, 173, 232, 44, 215, 117, 122, 235, 22, 11, 245, 89, 203, 95, 176, 156, 169, 81, 160, 127, 12, 246, 111, 23, 196, 73, 236, 216, 67, 31, 45, 164, 118, 123, 183, 204, 187, 62, 90, 251, 96, 177, 134, 59, 82, 161, 108, 170, 85, 41, 157, 151, 178, 135, 144, 97, 190, 220, 252, 188, 149, 207, 205, 55, 63, 91, 209, 83, 57, 132, 60, 65, 162, 109, 71, 20, 42, 158, 93, 86, 242, 211, 171, 68, 17, 146, 217, 35, 32, 46, 137, 180, 124, 184, 38, 119, 153, 227, 165, 103, 74, 237, 222, 197, 49, 254, 24, 13, 99, 140, 128, 192, 247, 112, 7]
+
+from time import *
+from random import *
+from math import *
+from multiprocessing import *
+from os import *
+
+
+def mod(a):
+    if (a < 0):
+        a = -a
+    
+    m = a // 8
+    t = a // (256**m)
+    a = a % (256**m)
+    while (m > 0):
+        t = multy(t, 27)
+        m = m - 1
+        t = t ^ a // (256**m)
+        a = a % (256**m)
+    return t
+
+def multy_Std(a, b):
+    # calculate by shifting and addtion
+    # input: two integers within 0 ~ 255
+    # output: an integer within 0 ~ 255
+    if (a < b):
+        a,b = b,a
+
+    s = 0
+    if (b):
+        if (b & 1):
+            s = a
+        b = b >> 1
+
+    while (b):
+        a = a << 1
+        c = a // 256
+        a = a % 256
+        if (c):
+            a = a ^ 0b11011
+        if (b & 1):
+            s = s ^ a
+        b = b >> 1
+
+    return s
+
+def multy_Double(a, b, c, d):
+    # calculate: (multy_Std(a, b), multy_Std(c, d))
+    if (a < b):
+        a,b = b,a
+    if (c < d):
+        c,d = d,c
+    a = (a << 16) ^ c
+    b = (b << 16) ^ d
+
+    s = 0
+    if (b):
+        x = b & 65537
+        if (x == 1):
+            s = a & 255
+        elif (x == 65536):
+            s = a & 16711680
+        elif (x == 65537):
+            s = a & 16711935
+        b = (b >> 1) & 16711935
+
+    while (b):
+        a = a << 1
+        y = a & 16777472
+        a = a & 16711935
+        if (y == 256):
+            a = a ^ 27
+        elif (y == 16777216):
+            a = a ^ 1769472
+        elif (y == 16777472):
+            a = a ^ 1769499
+        z = b & 65537
+        if (z == 1):
+            s = (a & 255)^ s
+        elif (z == 65536):
+            s = (a & 16711680)^ s
+        elif (z == 65537):
+            s = (a & 16711935)^ s
+        b = (b >> 1) & 16711935
+
+    return (s & 255, s & 16711680)
+
+def multy(a, b):
+    # calculate by listing
+    # input: two integers within 0 ~ 255
+    # output: an integer within 0 ~ 255
+    if (a == 0 or b == 0):
+        return 0
+    else:
+        return EXP[(LOG[a-1]+LOG[b-1]) % 255]
+
+def mulwithio(a, b):
+    # calculate by listing
+    # input: two strings of "0" and "1"
+    # output: a 8-bit string of "0", "1"
+    return tobin(multy(mod(todec(a)), mod((todec(b)))))
+
+def multy_SplitLoops(In, Out, start, end):
+    for k in range(start, end):
+        Out[k] = multy(In[k][0], In[k][1])
+
+def division(a, b):
+    a,b = mod(a),mod(b)
+    if (b == 0):
+        return 0
+    else:
+        l1 = len(bin(a)[2:])
+        l2 = len(bin(b)[2:])
+        l = l1 - l2
+        d = 0
+        while (not((l < 0) or (a == 0))):
+            c = b << l
+            d = d + (1 << l)
+            a = a ^ c
+            l1 = len(bin(a)[2:])
+            l = l1 - l2
+        return (d, a)
+
+def inverse(a):
+    return INV[a]
+
+def invwithio(a):
+    return tobin(inverse(mod(todec(a))))
+
+def sbox(a):
+    a = inverse(a)
+    u = 0
+    for i in AFF:
+        u = u ^ (i * (a & 1))
+        a = a >> 1
+    u = u ^ 99
+    return u
+
+def sbox_Linear_Analysis():
+    ## num to set numbers of eqautions
+    num = 10
+    
+    rank = []
+    counter = num + 1
+    for a in range(0, 256):
+        for b in range(0, 256):
+            bias = -128
+            for x in range(0, 256):
+                y = sbox(x)
+                u = (a & x) ^ (b & y)
+                v = 0
+                while (u != 0):
+                    v = v ^ (u & 1)
+                    u = u >> 1
+                if (v == 0):
+                    bias = bias + 1
+            if (counter > 0):
+                rank.append((tobin(a), tobin(b), bias))
+                counter = counter - 1
+            else:
+                for i in range(0, num + 1):
+                    if (abs(rank[i][2]) < abs(bias)):
+                        rank[i] = (tobin(a), tobin(b), bias)
+                        break
+
+    ## output
+    print str(num) + "equations with bigger possibility:"
+    print "(inputsum, outputsum, bias from 128[+ for 0, - for 1])"
+    for token in rank[1:]:
+        print "(%s, %s, %+d)" % token
+
+def todec(s):
+    # used when input
+    # s must be string type, not int type.
+    buf = ''
+    for i in s:
+        if (i != ' '): buf = buf + i
+    n = '0b' + buf
+    return int(n, 2)
+
+def tobin(n):
+    # used when output
+    # n must be int type, without useless '0' leading.
+    s = bin(n)[2:]
+    l = len(s)
+    if (l % 8 != 0):
+        l = (l // 8 + 1) * 8 - l
+    else:
+        l = 0
+    s = '0' * l + s
+    buf = ''
+    for i in range(0, len(s) // 8):
+        buf = buf + s[i*8: i*8+8] + ' '
+    return buf[0:-1]
+
+def testbench():
+    # testing time used for 100,000 multiplication and 100,000 inversion
+    print "Testbench runnning..."
+    C = []
+    D = []
+    print "Random numbers generating..."
+    for i in range(0, 100000):
+        C.append((randrange(0, 256, 1),randrange(0, 256, 1)))
+        D.append(randrange(0, 256, 1))
+    print "100,000 multiplication:"    
+    a = time()
+    for i, j in C:
+        multy(i, j)
+    print time() - a
+    print "100,000 inversion:"
+    a = time()
+    for i in D:
+        inverse(i)
+    print time() - a
+    print "Test ends."
+
+def testbench_SplitLoops():
+    # Target: Split a long loop to several shorter loops,
+    #         with calculating at the same time by multiprocesses.
+    
+    
+    # length: parameter for the length of input array, i.e. the length of loop
+    # the input array and the output array should have the same length
+    length = 100000
+    
+    In = []
+    Out = [0 for i in range(0, length)]
+
+    for i in range(0, length):
+        In.append((randrange(0, 256, 1), randrange(0, 256, 1)))
+
+    # seg: parameter for  the length of every shorter loop
+    # seg should be a factor of "length"
+    seg = 1000
+
+    print "Test for splitting loops --- length:%d, seg: %d" % (length, seg)
+    a = time()
+    pool = Pool(2)
+    for i in range(0, length, seg):
+        j = i + seg
+        pool.apply_async(multy_SplitCycles(In, Out, i, j))
+    pool.close()
+    print "Time used: %f" % (time() - a)
+    print "Test Ends."
+
+
+testbench()
